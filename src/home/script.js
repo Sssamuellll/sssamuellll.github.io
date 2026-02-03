399a7221cb43bf9c8a70fdab2687844e159d5a83
Samuel <139263675+Sssamuellll@users.noreply.github.com>
2026-02-03 12:58:36 +0000

    Page Layout pt5
    - Project items now have a background that the user can hover over and click on the project.
    - Project item hover now changes the background of the project item and adds an outline.
    - Page footer was implemented.
    - Page menu options was adjusted so that the user now hovers over the tab and text together as one element.

--- src/home/script.js
+++ src/home/script.js
@@ -1,17 +1,18 @@
 // Homepage JavaScript functionality:
 // Variables:
-// Grabbing all the elements that have the class attribute of projectImg.
-let projectImgs = document.querySelectorAll(".projectImg");
+// Grabbing all the elements that have the class attribute of projectItem.
+let projectItems = document.querySelectorAll(".projectItem");
 
 // Functions:
 
 // Clicking on projects:
-projectImgs.forEach((img) =>
+projectItems.forEach((item) =>
 {
-    img.addEventListener("click", function () 
+    // Adding event listener to listen for user clicks while the cursor is over the projectItem element:
+    item.addEventListener("click", function () 
     {
-        // Retrieves the image's id and uses it to transport the user to the corresponding page:
-        let imgId = img.getAttribute("id");
-        window.location.href = `../projects/${imgId}/${imgId}.html`;
+        // Retrieves the projectItem's image element id and uses it to transport the user to the corresponding page:
+        const itemId = item.children[0].getAttribute("id");
+        window.location.href = `../projects/${itemId}/index.html`;
     });
 });
--- src/siteScript.js
+++ src/siteScript.js
@@ -1,34 +1,37 @@
 // Site JavaScript Functionality:
 // Variables:
-// Grabbing all the elements that have the class attribute of menuTab.
-let menuLabels = document.querySelectorAll(".menuLabel");
+// Grabbing all the elements that have the class attribute of menuOption.
+let menuOptions = document.querySelectorAll(".menuOption");
 
 // Functions:
 // When called, changes the element color dependant on whether the mouse is hovering over it:
 function hover_over_tab(_element, _isHovering)
 {
-    let elementId = _element.getAttribute("id");
-    const tab = document.querySelector(`#${elementId}.menuTab`);
+    // Retrieves the menu option's tab visual and text as well as assigning the pointer value to cursor:
+    const tab = _element.children[0];
+    const text = _element.children[_element.children.length-1];
+    _element.style.cursor = "pointer";
 
     // Style changes are applied dependant on whether the user is hovering over the element:
     if(_isHovering)
     {
-        _element.style.color= "rgb(123, 224, 255)";
+        // When hovering: enlarge the tab and change the colour of the text:
         tab.style.fontSize = "5px";
-        _element.style.cursor = "pointer";
+        text.style.color = "rgb(123, 224, 255)";
     }
     else
     {
-        _element.style.color = "rgb(255, 255, 255)";
+        // When not hovering: reset both menuOption tab and text:
         tab.style.fontSize = "3px";
+        text.style.color = "rgb(255, 255, 255)";
     };
 };
 
 // Hovering over menu option tabs:
 // Assigning event listeners to all tabs:
-menuLabels.forEach((mLabel) =>
+menuOptions.forEach((mOption) =>
 {
     // Listening for mouse entering and leaving the element:
-    mLabel.addEventListener("mouseenter", function ()  {hover_over_tab(mLabel, true)}, false);
-    mLabel.addEventListener("mouseleave", function ()  {hover_over_tab(mLabel, false)}, false);
+    mOption.addEventListener("mouseenter", function ()  {hover_over_tab(mOption, true)}, false);
+    mOption.addEventListener("mouseleave", function ()  {hover_over_tab(mOption, false)}, false);
 });
